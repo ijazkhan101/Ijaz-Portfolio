c9c727fcb52900afafaae2c5644404e625e3f2b9
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-13 18:37:23 +0500

    push
    push

--- ijaz-web-projects/app/components/footer.tsx
+++ ijaz-web-projects/app/components/footer.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import Link from "next/link";
 
 
-export const Footer = () =>{
-    return( 
-        <div>
-            <div>
-         Footer
-            </div>
+export const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div className="flex">
+          <div className="w-1/3">Column 1</div>
+          <div className="w-1/3">Column 2</div>
+          <div className="w-1/3">
+          <Link href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon icon={faTwitter}  color="blue"  className="w-300 h-300"/>
+</Link>
 
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
